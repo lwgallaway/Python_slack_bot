0a308d5bf925ef6cbbfc49b60395217098bd72d8
Logan Gallaway <lwgallaway@users.noreply.github.com>
2018-06-16 10:53:35 -0400

    Create slack_bot.py

---
+++ slack_bot.py
@@ -0,0 +1,11 @@
+#python slack bot
+
+from slackclient import SlackClient
+ 
+slack_client = SlackClient("")  #put slack key here
+ 
+api_call = slack_client.api_call("users.list")
+if api_call.get('ok'):
+    users = api_call.get('members')
+    for user in users:
+        print user.get('name')
